48f1198a08612ccdbfc7b39a27ecbee309c5c7bc
Abdullah Atif <114088205+atif721@users.noreply.github.com>
2024-04-21 10:54:22 +0600

    sum of series 1+1 divide n.c

--- 06 Lab report 6/sum of series 1+1 divide n.c
+++ 06 Lab report 6/sum of series 1+1 divide n.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int term;
-    printf("Enter the number of terms: ");
-    scanf("%d", &term);
-
     float sum = 0;
 
-    for (int i = 1; i <= term; i++) {
+    printf("Enter the number of terms: ");
+    scanf("%d", &term);
+    printf("\n");
+
+    for (int i = 1; i <= term; i++)
+    {
+        printf("1/%d",i);
+        if(i<term)
+        {
+            printf(" + ");
+        }
         sum += 1.0 / i;
     }
 
-    printf("The sum of the series is: %f\n", sum);
+    printf("\n\nThe sum of the series is: %.3f\n", sum);
 
     return 0;
 }
