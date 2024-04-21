b8c3afe2a25f14345179bda748ef73ce8f2576b3
Abdullah Atif <114088205+atif721@users.noreply.github.com>
2024-04-21 09:43:43 +0600

    sum of series 1+11+111+1111+11111.c

--- 06 Lab report 6/sum of series 1+11+111+1111+11111.c
+++ 06 Lab report 6/sum of series 1+11+111+1111+11111.c
@@ -2,21 +2,26 @@
 
 int main()
 {
-    int term, multiplier,sum=0,term2=1,num=0;
+    int i,term, multiplier,sum=0,num=0;
     printf("Enter the number of terms: ");
     scanf("%d", &term);
-    printf("Enter the multiplier: ");
+    printf("\nEnter the multiplier: ");
     scanf("%d", &multiplier);
+    printf("\n");
 
-    for (int i = 1; i <= term; i++)
+    for (i = 1; i <= term; i++)
     {
         num = num * 10 + multiplier;
-        term2 = term2 * 10;
         sum += num;
-        printf("%d+",num);
+        printf("%d",num);
+
+        if(i<term)
+        {
+            printf("+");
+        }
     }
 
-    printf("\nThe sum of the series is: %d\n", sum);
+    printf("\n\nThe sum of the series is: %d\n", sum);
 
     return 0;
 }
