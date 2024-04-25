bf028a27f68fe4d6e857eeca914567b4457e1816
Abdullah Atif <114088205+atif721@users.noreply.github.com>
2024-04-25 21:19:56 +0600

    sum of all prime numbers in array.c

--- 07 Lab report 7/sum of all prime numbers in array.c
+++ 07 Lab report 7/sum of all prime numbers in array.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 #include<time.h>
 
-#define ARRAY_SIZE 10
+#define ARRAY_SIZE 20 // you can change the ARRAY_SIZE at your will.
 
 int main()
 {
     srand(time(NULL));
 
-    int i,j,prime,array[ARRAY_SIZE], sum=0;
+    int i,j,prime,array[ARRAY_SIZE], sum=0,True=1,False=0;
 
     for (i = 0; i < ARRAY_SIZE; i++)
     {
-        array[i] = rand()%10;
+        array[i] = rand()%50; // here rand()%* is not fixed you can change this range as your will.
     }
 
     printf("Random numbers in the array:\n");
@@ -22,27 +22,27 @@ int main()
         printf("array[%3d] = %d\n",i,array[i]);
     }
 
+    printf("\nPrime numbers are : \n");
     for (i = 0; i < ARRAY_SIZE; i++)
     {
-        prime = 1;
+        prime = True;
         for (j = 2; j <= array[i] / 2; j++)
         {
             if (array[i] % j == 0)
             {
-                prime = 0;
+                prime = False;
                 break;
             }
         }
 
         if (prime && array[i] > 1)
         {
+            printf("array[%3d] = %d\n",i,array[i]);
             sum += array[i];
         }
     }
 
-    printf("Sum of prime numbers in the array: %d\n", sum);
-
+    printf("\nSum of prime numbers in the array: %d\n", sum);
 
     return 0;
 }
-
